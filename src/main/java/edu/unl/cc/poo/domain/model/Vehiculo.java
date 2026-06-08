9145776c87c0dfe9cad34e5ec0a0799bb4388bdf
Danny <dannfern997@gmail.com>
2026-06-07 20:33:03 -0500

    Modificación de la clase Vehículo

--- src/main/java/edu/unl/cc/poo/domain/model/Vehiculo.java
+++ src/main/java/edu/unl/cc/poo/domain/model/Vehiculo.java
@@ -7,3 +7,58 @@ import edu.unl.cc.poo.domain.enums.TipoVehiculo;
  * Encapsula placa, conductor y tipo de vehiculo.
  */
 
+public class Vehiculo {
+
+    private String placa;
+    private String nombreConductor;
+    private TipoVehiculo tipoEspecifico;
+
+    public Vehiculo(String placa, String nombreConductor) {
+        this.placa = placa;
+        this.nombreConductor = nombreConductor;
+        this.tipoEspecifico = null;
+    }
+
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public TipoVehiculo getTipo() {
+        if (tipoEspecifico != null) {
+            return tipoEspecifico;
+        }
+        return determinarTipoPorPlaca();
+    }
+
+    public void setTipo(TipoVehiculo tipo) {
+        this.tipoEspecifico = tipo;
+    }
+
+    public String getNombreConductor() {
+        return nombreConductor;
+    }
+
+    public void setNombreConductor(String nombreConductor) {
+        this.nombreConductor = nombreConductor;
+    }
+
+    private TipoVehiculo determinarTipoPorPlaca() {
+        if (placa == null || placa.isBlank()) {
+            return TipoVehiculo.AUTOMOVIL;
+        }
+        String upper = placa.toUpperCase();
+        if (upper.startsWith("M")) return TipoVehiculo.MOTO;
+        if (upper.startsWith("C")) return TipoVehiculo.CAMIONETA;
+        return TipoVehiculo.AUTOMOVIL;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo{placa='" + placa + "', conductor='" + nombreConductor + "'}";
+    }
+}
